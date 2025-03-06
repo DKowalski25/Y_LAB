5b762e3045a54548543d44af3a8fdf4a549b82b6
DKowalski25 <dshinoda25@gmail.com>
2025-03-06 22:22:48 +0300

    Created transaction_input model

---
+++ PersonalFinancialTracker/src/main/java/dto/transaction/TransactionIn.java
@@ -0,0 +1,17 @@
+package dto.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class TransactionIn {
+    private String userId;
+    private double amount;
+    private String category;
+    private LocalDate date;
+    private String description;
+    private boolean isIncome;
+}
