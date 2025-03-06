e1435a30303401ca59e1ed47de15d3b46f472541
DKowalski25 <dshinoda25@gmail.com>
2025-03-07 01:56:29 +0300

    Created TransactionController

---
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/controller/transaction/TransactionControllerImpl.java
@@ -0,0 +1,34 @@
+package dev.personal.financial.tracker.controller.transaction;
+
+import dev.personal.financial.tracker.model.Transaction;
+
+import dev.personal.financial.tracker.service.transaction.TransactionService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class TransactionControllerImpl implements TransactionController {
+    private final TransactionService transactionService;
+
+    @Override
+    public void addTransaction(Transaction transaction) {
+        transactionService.addTransaction(transaction);
+    }
+
+    @Override
+    public Transaction getTransaction(String id) {
+        return transactionService.getTransactionById(id);
+    }
+
+    @Override
+    public List<Transaction> getTransactionsByUserId(String userId) {
+        return transactionService.getTransactionsByUserId(userId);
+    }
+
+    @Override
+    public void deleteTransaction(String id) {
+        transactionService.deleteTransaction(id);
+    }
+}
