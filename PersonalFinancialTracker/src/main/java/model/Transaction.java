c2553bdb17711e07b53845690d40a6e7e067cb2f
DKowalski25 <dshinoda25@gmail.com>
2025-03-06 22:22:33 +0300

    Created transaction model

---
+++ PersonalFinancialTracker/src/main/java/model/Transaction.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Transaction {
+    private String id;
+    private String userId;
+    private double amount;
+    private String category;
+    private LocalDate date;
+    private String description;
+    private boolean isIncome;
+}
