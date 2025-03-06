cf415215cbf7a3b44f1134d4d140b99741d5a2c4
DKowalski25 <dshinoda25@gmail.com>
2025-03-07 01:57:09 +0300

    Added logic for working with transactions

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/Main.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/Main.java
@@ -1,9 +1,15 @@
 package dev.personal.financial.tracker;
 
+import dev.personal.financial.tracker.controller.transaction.TransactionController;
+import dev.personal.financial.tracker.controller.transaction.TransactionControllerImpl;
 import dev.personal.financial.tracker.controller.user.UserController;
 import dev.personal.financial.tracker.controller.user.UserControllerImpl;
+import dev.personal.financial.tracker.repository.transaction.TransactionRepository;
+import dev.personal.financial.tracker.repository.transaction.TransactionRepositoryImpl;
 import dev.personal.financial.tracker.repository.user.UserRepository;
 import dev.personal.financial.tracker.repository.user.UserRepositoryImpl;
+import dev.personal.financial.tracker.service.transaction.TransactionService;
+import dev.personal.financial.tracker.service.transaction.TransactionServiceImpl;
 import dev.personal.financial.tracker.service.user.UserService;
 import dev.personal.financial.tracker.service.user.UserServiceImpl;
 import dev.personal.financial.tracker.utils.ConsoleInterface;
@@ -14,7 +20,11 @@ public class Main {
         UserService userService = new UserServiceImpl(userRepository);
         UserController userController = new UserControllerImpl(userService);
 
-        ConsoleInterface consoleInterface = new ConsoleInterface(userController);
+        TransactionRepository transactionRepository = new TransactionRepositoryImpl();
+        TransactionService transactionService = new TransactionServiceImpl(transactionRepository);
+        TransactionController transactionController = new TransactionControllerImpl(transactionService);
+
+        ConsoleInterface consoleInterface = new ConsoleInterface(userController, transactionController);
         consoleInterface.run();
     }
 }
