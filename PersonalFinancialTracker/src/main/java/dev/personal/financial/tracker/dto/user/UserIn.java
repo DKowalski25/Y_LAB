614a5d20d474c1e680e26d22a74353b78d529d57
DKowalski25 <dshinoda25@gmail.com>
2025-03-08 00:32:04 +0300

    Changed user role type from String to UserRole class

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/user/UserIn.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/user/UserIn.java
@@ -1,13 +1,16 @@
 package dev.personal.financial.tracker.dto.user;
 
+import dev.personal.financial.tracker.model.UserRole;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UserIn {
+    private String id;
     private String name;
     private String email;
     private String password;
-    private String role;
+    private UserRole role;
 }
