286547bc62276bdea57fef6621caaea76406e1a4
DKowalski25 <dshinoda25@gmail.com>
2025-03-08 00:32:48 +0300

    Added NoArgsConstructor, will remove it later

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/model/User.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/model/User.java
@@ -2,9 +2,11 @@ package dev.personal.financial.tracker.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String id;
     private String name;
