ee83305db04a347a46541c90d681bca3980707a9
DKowalski25 <dshinoda25@gmail.com>
2025-03-08 21:34:07 +0300

    Created Application

---
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/Application.java
@@ -0,0 +1,77 @@
+package dev.personal.financial.tracker;
+
+import dev.personal.financial.tracker.UI.ConsoleInterface;
+import dev.personal.financial.tracker.controller.admin.AdminController;
+import dev.personal.financial.tracker.controller.budget.BudgetController;
+import dev.personal.financial.tracker.controller.goal.GoalController;
+import dev.personal.financial.tracker.controller.transaction.TransactionController;
+import dev.personal.financial.tracker.controller.user.UserController;
+import dev.personal.financial.tracker.model.User;
+import dev.personal.financial.tracker.model.UserRole;
+import dev.personal.financial.tracker.repository.admin.AdminRepository;
+import dev.personal.financial.tracker.repository.budget.BudgetRepository;
+import dev.personal.financial.tracker.repository.goal.GoalRepository;
+import dev.personal.financial.tracker.repository.transaction.TransactionRepository;
+import dev.personal.financial.tracker.repository.user.UserRepository;
+import dev.personal.financial.tracker.service.admin.AdminService;
+import dev.personal.financial.tracker.service.budget.BudgetService;
+import dev.personal.financial.tracker.service.goal.GoalService;
+import dev.personal.financial.tracker.service.transaction.TransactionService;
+import dev.personal.financial.tracker.service.user.UserService;
+import dev.personal.financial.tracker.util.DependencyInjector;
+
+public class Application {
+
+    private static final Application INSTANCE = new Application();
+
+    private Application() {}
+
+    public static Application getInstance() {
+        return INSTANCE;
+    }
+
+    public void run() {
+        DependencyInjector injector = new DependencyInjector();
+
+        UserRepository userRepository = injector.createUserRepository();
+        UserService userService = injector.createUserService(userRepository);
+        UserController userController = injector.createUserController(userService);
+
+        TransactionRepository transactionRepository = injector.createTransactionRepository();
+        TransactionService transactionService = injector.createTransactionService(transactionRepository);
+        TransactionController transactionController = injector.createTransactionController(transactionService);
+
+        GoalRepository goalRepository = injector.createGoalRepository();
+        GoalService goalService = injector.createGoalService(goalRepository);
+        GoalController goalController = injector.createGoalController(goalService);
+
+        BudgetRepository budgetRepository = injector.createBudgetRepository();
+        BudgetService budgetService = injector.createBudgetService(budgetRepository);
+        BudgetController budgetController = injector.createBudgetController(budgetService);
+
+        AdminRepository adminRepository = injector.createAdminRepository(userRepository);
+        AdminService adminService = injector.createAdminService(adminRepository);
+        AdminController adminController = injector.createAdminController(adminService);
+
+        ConsoleInterface consoleInterface = new ConsoleInterface(
+                userController,
+                userRepository,
+                transactionController,
+                goalController,
+                budgetController,
+                adminController
+        );
+
+        User admin = new User(
+                "admin",
+                "Admin",
+                "a@admin.com",
+                "123456",
+                UserRole.ADMIN,
+                false
+        );
+
+        userRepository.save(admin);
+        consoleInterface.run();
+    }
+}
