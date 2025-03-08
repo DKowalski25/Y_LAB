f85641a3cd2557c96abc60c2f9a8fb2a2c3a9240
DKowalski25 <dshinoda25@gmail.com>
2025-03-09 00:11:30 +0300

    Refactor

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/controller/budget/BudgetController.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/controller/budget/BudgetController.java
@@ -2,7 +2,6 @@ package dev.personal.financial.tracker.controller.budget;
 
 import dev.personal.financial.tracker.dto.budget.BudgetIn;
 import dev.personal.financial.tracker.dto.budget.BudgetOut;
-import dev.personal.financial.tracker.model.Budget;
 
 public interface BudgetController {
     void setBudget(BudgetIn budgetIn);
--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/controller/budget/BudgetControllerImpl.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/controller/budget/BudgetControllerImpl.java
@@ -2,7 +2,6 @@ package dev.personal.financial.tracker.controller.budget;
 
 import dev.personal.financial.tracker.dto.budget.BudgetIn;
 import dev.personal.financial.tracker.dto.budget.BudgetOut;
-import dev.personal.financial.tracker.model.Budget;
 import dev.personal.financial.tracker.service.budget.BudgetService;
 
 import lombok.RequiredArgsConstructor;
