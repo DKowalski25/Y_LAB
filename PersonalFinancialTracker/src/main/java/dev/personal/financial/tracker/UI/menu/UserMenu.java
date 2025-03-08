a5eebb0cdf80048f0bd97b0c4431a61b0c7bd568
DKowalski25 <dshinoda25@gmail.com>
2025-03-08 22:23:18 +0300

    Added new logic for update and delete user

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/UI/menu/UserMenu.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/UI/menu/UserMenu.java
@@ -3,6 +3,7 @@ package dev.personal.financial.tracker.UI.menu;
 import dev.personal.financial.tracker.UI.handler.BudgetHandler;
 import dev.personal.financial.tracker.UI.handler.GoalHandler;
 import dev.personal.financial.tracker.UI.handler.TransactionHandler;
+import dev.personal.financial.tracker.UI.handler.UserHandler;
 import dev.personal.financial.tracker.util.ConsolePrinter;
 import dev.personal.financial.tracker.dto.user.UserOut;
 
@@ -13,6 +14,7 @@ public class UserMenu {
     private final TransactionHandler transactionHandler;
     private final GoalHandler goalHandler;
     private final BudgetHandler budgetHandler;
+    private final UserHandler userHandler;
     private final ConsolePrinter printer;
 
     public void run(UserOut userOut) {
@@ -24,7 +26,9 @@ public class UserMenu {
             printer.printPrompt("4. Просмотреть цели");
             printer.printPrompt("5. Установить бюджет");
             printer.printPrompt("6. Просмотреть бюджет");
-            printer.printPrompt("7. Выйти из аккаунта");
+            printer.printPrompt("7. Редактировать профиль");
+            printer.printPrompt("8. Удалить аккаунт");
+            printer.printPrompt("9. Выйти из аккаунта");
 
             int choice = printer.readInt("Выберите номер действие:");
 
@@ -48,6 +52,12 @@ public class UserMenu {
                     budgetHandler.viewBudget(userOut);
                     break;
                 case 7:
+                    userHandler.updateProfile(userOut.getEmail());
+                    break;
+                case 8:
+                    userHandler.deleteAccount(userOut.getEmail());
+                    return;
+                case 9:
                     printer.printInfo("Вы вышли из аккаунта.");
                     return;
                 default:
