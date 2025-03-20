e902050997e9b9781e3c7d75dac3b7682461f4ad
DKowalski25 <dshinoda25@gmail.com>
2025-03-20 23:23:25 +0300

    Created Enum for transactions category

---
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/model/TransactionCategory.java
@@ -0,0 +1,15 @@
+package dev.personal.financial.tracker.model;
+
+public enum TransactionCategory {
+    FOOD,
+    CLOTHES,
+    HOUSING,
+    TRANSPORT,
+    HEALTH,
+    EDUCATION,
+    ENTERTAINMENT,
+    APPLIANCES,
+    PET,
+    SUBSCRIPTIONS,
+    OTHER;
+}
