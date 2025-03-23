ee6c9deb8c9a2e5ff5dde8e6ea1b0a20940a3f08
DKowalski25 <dshinoda25@gmail.com>
2025-03-23 23:09:47 +0300

    Refactored

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/util/EnvReader.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/util/EnvReader.java
@@ -3,12 +3,24 @@ package dev.personal.financial.tracker.util;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvReader {
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv dotenv;
+
+    static {
+        dotenv = Dotenv.configure()
+                .directory("../")
+                .filename(".env")
+                .ignoreIfMissing()
+                .load();
+
+        System.out.println("✅ .env файл загружен!");
+    }
 
     public static String getEnv(String key) {
         String value = dotenv.get(key);
         if (value == null) {
-            System.out.println("⚠ Environment variable " + key + " is not in .env file");
+            System.out.println("⚠ Переменная " + key + " отсутствует в .env");
+        } else {
+            System.out.println("🔹 Переменная " + key + " = " + value);
         }
         return value;
     }
