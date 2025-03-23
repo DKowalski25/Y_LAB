2a55f5a7030d3189fcb7c27cf2a500edfbf32011
DKowalski25 <dshinoda25@gmail.com>
2025-03-23 23:05:43 +0300

    Added validation annotations to BudgetIn

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/budget/BudgetIn.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/budget/BudgetIn.java
@@ -1,5 +1,7 @@
 package dev.personal.financial.tracker.dto.budget;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,11 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class BudgetIn {
     private int id;
+
+    @NotNull(message = "User ID is required")
     private int userId;
+
+    @NotNull(message = "Monthly budget is required")
+    @Min(value = 0, message = "Monthly budget must be greater than or equal to 0")
     private BigDecimal monthlyBudget;
 }
