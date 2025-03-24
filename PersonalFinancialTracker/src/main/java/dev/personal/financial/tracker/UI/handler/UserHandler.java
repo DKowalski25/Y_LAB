4271b3d53b2e208a3e412c9d48400e49b79d6927
DKowalski25 <dshinoda25@gmail.com>
2025-03-24 22:32:01 +0300

    Refactored

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/UI/handler/UserHandler.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/UI/handler/UserHandler.java
@@ -6,7 +6,6 @@ import dev.personal.financial.tracker.controller.user.UserController;
 import dev.personal.financial.tracker.dto.user.UserIn;
 import dev.personal.financial.tracker.dto.user.UserOut;
 import dev.personal.financial.tracker.model.User;
-import dev.personal.financial.tracker.model.UserRole;
 import dev.personal.financial.tracker.repository.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
