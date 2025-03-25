1c42412078787c86e74e7d4854de74aa8a387c9e
DKowalski25 <dshinoda25@gmail.com>
2025-03-25 21:49:08 +0300

    Added empty constructor

--- PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/budget/BudgetIn.java
+++ PersonalFinancialTracker/src/main/java/dev/personal/financial/tracker/dto/budget/BudgetIn.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BudgetIn {
     private int id;
 
